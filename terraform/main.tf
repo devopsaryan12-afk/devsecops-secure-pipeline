7918378023b0d48cf8364f88bee4f94fe5f9233c
devopsaryan12-afk <devops.aryan12@gmail.com>
2026-02-28 21:11:38 +0530

    Phase 6: Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "practice_vpc" {
 resource "aws_subnet" "practice_subnet" {
   vpc_id     = aws_vpc.practice_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
